d5e18761aef12adce057093892c8e65ddd2a745e
ToinNetuh <gpantonioneto@gmail.com>
2021-01-12 16:08:37 -0300

    Nomer

--- src/help.js
+++ src/help.js
@@ -1,7 +1,7 @@
-const help = (prefix, pushname, botname, oname) => {
+const help = (prefix, sender, pushname, botname, oname) => {
 	return `
 「 *Toin Bot* 」
-Oiin ${pushname}
+Oiin *@${sender.split("@s.whatsapp.net")[0]}
 
 ◪ *INFORMAÇÕES*
   ❏ Prefix: 「  ${prefix}  」
